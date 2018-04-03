359598e0d60b70803584cd56a7e28d878c7dcb3a
Yashwanth <yyellapragada@gmail.com>
2018-04-03 19:12:37 +1000

    [Yashwanth/Kathy][#0] Add names Yashwanth and kathy to team page

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -1,6 +1,6 @@
-// const TeamPage = require('../../src/js/pages/teamPage');
+const TeamPage = require('../../src/js/pages/teamPage');
 
-/* describe('TeamPage', () => {
+describe('TeamPage', () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
@@ -12,20 +12,13 @@
       const page = new TeamPage();
       expect(page.template()).toContain("<h1>Made by:</h1>");
     });
-  });
-
-}); */
-
-const TeamPage = require('../../src/js/pages/teamPage');
-
-describe('#template', () => {
-    it('should have a template', () => {
-      const page = new TeamPage();
-      expect(page.template()).toContain("<h1>Made by:</h1>");
-    });
 
     it('should contain team members names', () => {
       const page = new TeamPage();
       expect(page.template()).toContain("Sirani McNeil");
+      expect(page.template()).toContain("Yashwanth");
+      expect(page.template()).toContain("Kathy");
     });
   });
+
+});

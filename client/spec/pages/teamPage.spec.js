8bd910070ec88e2e5ab83eeb95e85b6e58db6d48
Hannah Bourke <hannahbourke0@gmail.com>
2018-04-10 18:27:52 +1000

    Add Hannah to team page and update test

--- client/spec/pages/teamPage.spec.js
+++ client/spec/pages/teamPage.spec.js
@@ -1,24 +1,25 @@
-const TeamPage = require('../../src/js/pages/teamPage');
+const TeamPage = require("../../src/js/pages/teamPage");
 
-describe('TeamPage', () => {
+describe("TeamPage", () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-    watchFace = document.getElementById('watch-face');
+    watchFace = document.getElementById("watch-face");
   });
 
-  describe('#template', () => {
-    it('should have a template', () => {
+  describe("#template", () => {
+    it("should have a template", () => {
       const page = new TeamPage();
       expect(page.template()).toContain("<h1>Made by:</h1>");
     });
 
-    it('should contain team members names', () => {
+    it("should contain team members names", () => {
       const page = new TeamPage();
       expect(page.template()).toContain("Sirani McNeil");
       expect(page.template()).toContain("Yashwanth");
       expect(page.template()).toContain("Kathy");
       expect(page.template()).toContain("Agnes");
+      expect(page.template()).toContain("Hannah");
     });
-  });  
+  });
 });

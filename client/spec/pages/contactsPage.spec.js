5e894f147b069f4e5e3552de682fbee0cfff8687
Hannah Bourke <hannahbourke0@gmail.com>
2018-05-01 18:59:48 +1000

    [Hannah/Agnes/Kathy][#1] Fix failing test on contacts page

--- client/spec/pages/contactsPage.spec.js
+++ client/spec/pages/contactsPage.spec.js
@@ -1,40 +1,37 @@
-const ContactsPage = require('../../src/js/pages/contactsPage');
+const ContactsPage = require("../../src/js/pages/contactsPage");
 
-describe('ContactsPage', () => {
+describe("ContactsPage", () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-    watchFace = document.getElementById('watch-face');
+    watchFace = document.getElementById("watch-face");
   });
 
-  describe('#template', () => {
-    it('should have a template', () => {
+  describe("#template", () => {
+    it("should have a template", () => {
       const page = new ContactsPage();
-      expect(page.template()).toContain("<h1>Contacts</h1>");
+      expect(page.template()).toContain("<h1>Primary Contact</h1>");
     });
 
-    it('should have a template with specific contacts', () => {
-      const contacts = [
-        {name: 'hi', phoneNumber: '1234'},
-      ];
-      const props = { contacts }
+    it("should have a template with specific contacts", () => {
+      const contacts = [{ name: "hi", phoneNumber: "1234" }];
+      const props = { contacts };
       const page = new ContactsPage(props);
       expect(page.template()).toContain("<span>Name: hi</span>");
       expect(page.template()).toContain("<span>Phone: 1234</span>");
     });
   });
 
-  describe('#leftButtonEvent', () => {
-    it('goes to root page', () => {
+  describe("#topButtonEvent", () => {
+    it("goes to root page", () => {
       const props = {
-        navigate: () => { },
+        navigate: () => {}
       };
       const page = new ContactsPage(props);
-      spyOn(page, 'navigate');
+      spyOn(page, "navigate");
 
-      page.leftButtonEvent();
-      expect(page.navigate).toHaveBeenCalledWith('/');
+      page.topButtonEvent();
+      expect(page.navigate).toHaveBeenCalledWith("/");
     });
   });
-
 });
